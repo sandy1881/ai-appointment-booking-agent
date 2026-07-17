4f0288d31244674d7dd31def7f2b713e445ca190
Sandy <sandy@Sandys-MacBook-Air.local>
2026-07-17 20:18:58 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/brightcare_clinic/appointment_agent/AppointmentAgentApplication.java
@@ -0,0 +1,13 @@
+package com.brightcare_clinic.appointment_agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppointmentAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppointmentAgentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brightcare_clinic/appointment_agent/AppointmentAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brightcare_clinic.appointment_agent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
